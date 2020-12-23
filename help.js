19b688ab2b8fb711ddbdfd4be513da415336c055
man-x199 <74522163+man-x199@users.noreply.github.com>
2020-12-23 10:04:43 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,81 @@
+
+const help = (prefix) => { 
+	return `>𝙋𝙪𝙙𝙞𝙙𝙞𝙓𝘽𝙤𝙏 <
+
+*Status* : ```Online```
+*Groups* : ```13```
+*User* : ```23```
+*Fitur* : ```41```
+*Prefix* : (!)
+
+┏━━━°☛ ❬ 𝘼𝘽𝙊𝙐𝙏 ❭ ☚°━━━┓
+┃
+┏➣ *${prefix}owner*
+┣➣ *${prefix}donasi*
+┗➣ *${prefix}info*
+┃
+┣━━━°☛ ❬ 𝗠𝗔𝗞𝗘𝗥 ❭ ☚°━━━⊱
+┃
+┣➣ *${prefix}sticker*
+┣➣ *${prefix}tsticker*
+┣➣ *${prefix}nulis*
+┃
+┣━━━━°☛ ❬ 𝙈𝙀𝘿𝙄𝘼 ❭ ☚°━━━⊱
+┃
+┣➣ *${prefix}tts*
+┣➣ *${prefix}tiktok*
+┣➣ *${prefix}meme*
+┣➣ *${prefix}memeindo*
+┣➣ *${prefix}nsfwloli*
+┣➣ *${prefix}ocr*
+┣➣ *${prefix}loli*
+┃
+┣━━━°☛ ❬ 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 ❭ ☚°━━⊱
+┃
+┣➣ *yt* [link]
+┣➣ *play* [judul lagu]
+┃
+┣━━━━°☛ ❬ 𝙂𝙍𝙊𝙐𝙋 ❭ ☚°━━━━⊱
+┃
+┣➣ *${prefix}add* [62xxx]
+┣➣ *${prefix}kick* [tag]
+┣➣ *${prefix}setpp*
+┣➣ *${prefix}demote* [tag]
+┣➣ *${prefix}promote* [tag]
+┣➣ *${prefix}setpp*
+┣➣ *${prefix}group* [buka/tutup]
+┣➣ *${prefix}welcome* [1/0]
+┣➣ *${prefix}nsfw* [1/0]
+┣➣ *${prefix}simih* [1/0]
+┃
+┣━━━━━°☛ ❬ 𝙊𝙒𝙉𝙀𝙍 ❭ ☚°━━━━⊱
+┃
+┣➣ *${prefix}bc* 
+┣➣ *${prefix}leave*
+┣➣ *${prefix}clearall*
+┣➣ *${prefix}setprefix*
+┣➣ *${prefix}clone* [tag]
+┣➣ *${prefix}block*
+┣➣ *${prefix}unblock*
+┣➣ *${prefix}getses*
+┃
+┣━━━━°☛ ❬ 𝙊𝙏𝙃𝙀𝙍 ❭ ☚°━━━━━⊱
+┃
+┣➣ *${prefix}ytsearch*
+┣➣ *${prefix}listadmin*
+┣➣ *${prefix}blocklist*
+┣➣ *${prefix}simi*
+┣➣ *${prefix}wait*
+┣➣ *${prefix}fitnah*
+┣➣ *${prefix}tiktokstalk*
+┣➣ *${prefix}url2img*
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃🔹𝙏𝙃𝙄𝙎 𝘽𝙊𝙏 𝘽𝙔 𝘼𝙍𝙈𝘼𝙉𝙏𝙊𝘿🔹
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+exports.help = help
+
+
+
+  
